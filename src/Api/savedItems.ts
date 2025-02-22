884d7018587d4171746448f18373b6ad1756c1b4
SachithaEshan <eshanssg@gmail.com>
2025-02-22 15:11:28 +0530

    change API's for SavedItem

--- src/Api/savedItems.ts
+++ src/Api/savedItems.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import { isAuthenticated } from '../Api/middleware/authentication-middleware';
+import express, { RequestHandler } from 'express';
+import { isAuthenticated } from './middleware/authentication-middleware';
 import { addFavorite, removeFavorite, getFavorites } from '../Application/savedItems';
 
 const router = express.Router();
 
-router.get('/', isAuthenticated, getFavorites);
-router.post('/', isAuthenticated, addFavorite);
-router.delete('/:id', isAuthenticated, removeFavorite);
+router.get('/', isAuthenticated, getFavorites as RequestHandler);
+router.post('/', isAuthenticated, addFavorite as RequestHandler);
+router.delete('/:id', isAuthenticated, removeFavorite as RequestHandler);
 
 export default router;
